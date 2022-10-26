aeddd8bf89fcb855810e3e699901cdc07e8dea87
slepec <slepec2018@gmail.com>
2022-10-26 14:25:52 +0100

    home_task_1

--- build/js/main.js
+++ build/js/main.js
@@ -12,3 +12,20 @@ function randomNumber(min, max) {
 }
 
 randomNumber(10, 20);
+
+
+function randomNumberPoint(min, max, point) {
+  if (min < 0 || max < 0) {
+		return NaN;
+	} else if (min >= max) {
+		return NaN;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  let int = Math.random() * (max - min + 1) + min;
+  return Number(int.toFixed(point));
+}
+
+console.log(randomNumberPoint(10, 20, 3));
