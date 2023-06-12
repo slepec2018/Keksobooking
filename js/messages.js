b2ab657ee2cafee607d192dfb25d1ce767f08a07
slepec2018 <slepec2018@users.noreply.github.com>
2023-06-12 13:23:28 +0000

    deploy: e63c4702ce0531ee15b7997df8947a037f35ecf4

--- js/messages.js
+++ js/messages.js
@@ -6,22 +6,26 @@ const messError = document.querySelector('#error').content.querySelector('.error
 const showMesConfirm = () => {
 	const clone = messSuccess.cloneNode(true);
 
+	let popup = null;
+
 	// Событие удаления попапа по нажатия кнопки Esc
 	document.addEventListener('keydown', (event) => {
-		if (event.code === 'Escape' && projectBody.lastChild.classList.contains('success')) {
-			projectBody.lastChild.remove();
+		if (event.code === 'Escape' && popup.classList.contains('success')) {
+			popup.remove();
 		}
 	});
 
 	// Событие удаления попапа по нажатию мышки на любом месте
 	document.addEventListener('click', () => {
-		if (projectBody.lastChild.classList.contains('success')) {
+		if (popup.classList.contains('success')) {
 			// console.log(event.target.classList.contains('success'));
-			projectBody.lastChild.remove();
+			popup.remove();
 		}
 	});
 
 	projectBody.append(clone);
+
+	popup = projectBody.lastChild;
 };
 
 // Функция открытия попапа сообщения о ошибки загрузки данных на сервер
@@ -29,26 +33,30 @@ const showMessError = () => {
 	const clone = messError.cloneNode(true);
 	const messErrorButton = clone.querySelector('.error__button');
 
+	let popup = null;
+
 	// Событие удаления попапа сообщения о ошибке через кнопку
 	messErrorButton.addEventListener('click', () => {
-		projectBody.lastChild.remove();
+		popup.remove();
 	});
 
 	// Событие удаления попапа по нажатия кнопки Esc
 	document.addEventListener('keydown', (event) => {
-		if (event.code === 'Escape' && projectBody.lastChild.classList.contains('error')) {
-			projectBody.lastChild.remove();
+		if (event.code === 'Escape' && popup.classList.contains('error')) {
+			popup.remove();
 		}
 	});
 
 	// Событие удаления попапа по нажатию мышки на любом месте
 	document.addEventListener('click', () => {
-		if (projectBody.lastChild.classList.contains('error')) {
-			projectBody.lastChild.remove();
+		if (popup.classList.contains('error')) {
+			popup.remove();
 		}
 	});
 
 	projectBody.append(clone);
+
+	popup = projectBody.lastChild;
 };
 
 export { showMesConfirm, showMessError };
